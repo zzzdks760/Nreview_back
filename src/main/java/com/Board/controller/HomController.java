bc907e13e5f8fca4b88b82cd70c57528d3748511
zzzdks760 <zzzdks760@naver.com>
2023-01-04 17:57:33 +0900

    회원가입 로그인 회원목록 이메일 중복확인

---
+++ src/main/java/com/Board/controller/HomController.java
@@ -0,0 +1,13 @@
+package com.Board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomController {
+    // 기본페이지 요청 메서드
+    @GetMapping("/")
+    public String index() {
+        return "index"; // => templates 폴더의 index.html을 찾아감.
+    }
+}
